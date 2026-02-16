c36239e8955d741cb6aa1357193cc2aad1f33e20
sankarayougisrivastewar-sudo <sankarayougisrivastewar@gmail.com>
2026-02-16 15:00:24 +0530

    added something

---
+++ 05-special variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo "All args passsed to script:$@"
+echo "NUMBERS of vars Passed to script: $#"
+echo "script name : $0"
+echo "present directory: $PWD"
+echo "who is running : $USER"
+echo "home directory : $HOME"
+echo "PID of the Script: $$"
+echo "PID of the resently executed back ground process: $"
