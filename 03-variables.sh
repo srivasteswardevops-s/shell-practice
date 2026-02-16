6329839616e49d1f67451affd5c12d6563e29103
sankarayougisrivastewar-sudo <sankarayougisrivastewar@gmail.com>
2026-02-16 14:33:35 +0530

    added something

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=sunny
+PERSON2=sonu
+
+echo "$PERSON1:: Hello $PERSON2,how are you?"
+echo "$PERSON2:: Hi $PERSON2,I am fine thanks for asking."
+echo "$PERSON1:: what are you learinig?"
+echo  "$PERSON2:: I am learnnig shell scripting"
