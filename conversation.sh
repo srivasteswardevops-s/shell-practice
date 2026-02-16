48400786ef28831df1e74f2950ec2ef944c418bd
sankarayougisrivastewar-sudo <sankarayougisrivastewar@gmail.com>
2026-02-16 14:20:55 +0530

    added something

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+echo "Suresh:: Hello ramesh,how are you?"
+echo "Ramesh:: Hi suresh,I am fine thanks for asking."
+echo "Suresh:: what are you learinig?"
+echo  "ramesh:: I am learnnig shell scripting"
